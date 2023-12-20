515122a275c367dce1b8102c864e807223fdebdb
Roman Makeev <makeevrserg@gmail.com>
2023-12-20 05:35:46 +0300

    add lazy to Single

--- kdi/src/commonMain/kotlin/ru/astrainteractive/klibs/kdi/Single.kt
+++ kdi/src/commonMain/kotlin/ru/astrainteractive/klibs/kdi/Single.kt
@@ -4,11 +4,9 @@ package ru.astrainteractive.klibs.kdi
  * [Single] is a singleton value which will be a unique and single instant
  *
  * This can be helpful for repository storing, DB connection and etc
+ *
+ * Also implements [Lazy]
  */
-class Single<out T>(factory: Factory<T>) : Dependency<T> {
-    private val instance by lazy {
-        factory.create()
-    }
-    override val value: T
-        get() = instance
-}
+class Single<out T>(
+    factory: Factory<T>
+) : Dependency<T>, Lazy<T> by lazy(initializer = { factory.create() })
